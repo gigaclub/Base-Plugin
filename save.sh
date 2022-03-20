7b37690101b04d7cd33f16562e5fe1a2e91e6c93
Kevin <xxkevtvxx@gmail.com>
2022-03-20 18:16:06 +0100

    updated Plugin to 1.18.2

--- save.sh
+++
@@ -1,2 +0,0 @@
-cp base-paper/build/libs/base-paper-all.jar ../test_minecraft_server/plugins/base-paper.jar
-cp base-velocity/build/libs/base-velocity-all.jar ../test_velocity/plugins/base-velocity.jar
